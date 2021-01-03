833bc452adc008688b1fe02923a5ef5122d63255
Abishty <18abhishek.b@gmail.com>
2021-01-03 23:09:52 +0530

    fetch product api created and call in frontend

--- bishtkart_frontend/src/screens/HomeScreen.js
+++ bishtkart_frontend/src/screens/HomeScreen.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "../components/Product";
-import DATA from "../data";
+import MessageBox from "../components/MessageBox";
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    getProducts();
+  }, []);
   return (
-      <div className="row center">
-        {DATA.products.map((product) => {
-          return <Product key={product._id} product={product} />;
-        })}
-      </div>
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => {
+            return <Product key={product._id} product={product} />;
+          })}
+        </div>
+      )}
+    </div>
   );
 }
